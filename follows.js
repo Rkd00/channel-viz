c96a95f47c5baf183ae86d31520e849efde95f2c
Rkd00 <daeron.ag@gmail.com>
2013-10-02 12:20:37 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'awD4kpTj4vAAlO9QcZeIiNq8xPqSAKxKN3U4V2JaMVZnYz0g', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [546021914, 1419237083], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
